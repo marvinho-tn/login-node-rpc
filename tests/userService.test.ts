9e53af13bdb26f5405e2c43b26214cbdc8521731
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-11-28 20:52:16 +0000

    criação dos testes com jest

--- tests/userService.test.ts
+++ tests/userService.test.ts
@@ -1,68 +1,54 @@
 import { ERRORS, MESSAGES } from '../src/common/utils/constants';
-import { expect } from 'chai';
 import ILoginService from '../src/modules/auth/domain/interfaces/loginService';
 import IUserRepository from '../src/modules/auth/domain/interfaces/userRepository';
 import LoginService from '../src/modules/auth/domain/services/loginService';
 import Result from '../src/modules/auth/domain/models/result';
-import sinon from 'sinon';
-import UserRepository from '../src/modules/auth/infra/repositories/userRepository';
+
+jest.mock('../src/modules/auth/infra/repositories/userRepository'); // Mocka a classe UserRepository
 
 describe('LoginService', () => {
-  let userRepositoryMock: sinon.SinonMock;
+  let userRepositoryMock: jest.Mocked<IUserRepository>;
   let loginService: ILoginService;
 
   beforeEach(() => {
-    userRepositoryMock = sinon.mock(UserRepository.prototype);
-    loginService = new LoginService(userRepositoryMock as unknown as IUserRepository);
-  });
+    userRepositoryMock = {
+      getUserByName: jest.fn(),
+    } as jest.Mocked<IUserRepository>;
 
-  afterEach(() => {
-    sinon.restore();
+    loginService = new LoginService(userRepositoryMock);
   });
 
   it('Deve realizar login com sucesso', async () => {
     const validUser = { username: 'validuser', password: 'validpassword' };
 
-    userRepositoryMock.expects('getUserByName')
-      .withArgs(validUser.username)
-      .returns(Promise.resolve(validUser));
+    userRepositoryMock.getUserByName.mockResolvedValue(validUser);
 
     const result: Result = await loginService.login(validUser.username, validUser.password);
 
-    expect(result.success).to.be.true;
-    expect(result.message).to.equal(MESSAGES.LOGIN_SUCCESS);
-
-    userRepositoryMock.verify();
+    expect(result.success).toBe(true);
+    expect(result.message).toBe(MESSAGES.LOGIN_SUCCESS);
   });
 
   it('Deve falhar no login com senha incorreta', async () => {
     const validUser = { username: 'validuser', password: 'validpassword' };
     const invalidPassword = 'wrongpassword';
 
-    userRepositoryMock.expects('getUserByName')
-      .withArgs(validUser.username)
-      .returns(Promise.resolve(validUser));
+    userRepositoryMock.getUserByName.mockResolvedValue(validUser);
 
     const result: Result = await loginService.login(validUser.username, invalidPassword);
 
-    expect(result.success).to.be.false;
-    expect(result.message).to.equal(ERRORS.INVALID_CREDENTIALS);
-
-    userRepositoryMock.verify();
+    expect(result.success).toBe(false);
+    expect(result.message).toBe(ERRORS.INVALID_CREDENTIALS);
   });
 
   it('Deve falhar no login com nome de usuário incorreto', async () => {
     const invalidUser = { username: 'wronguser', password: 'validpassword' };
 
-    userRepositoryMock.expects('getUserByName')
-      .withArgs(invalidUser.username)
-      .returns(Promise.resolve(null));
+    userRepositoryMock.getUserByName.mockResolvedValue(null);
 
     const result: Result = await loginService.login(invalidUser.username, invalidUser.password);
 
-    expect(result.success).to.be.false;
-    expect(result.message).to.equal(ERRORS.INVALID_CREDENTIALS);
-
-    userRepositoryMock.verify();
+    expect(result.success).toBe(false);
+    expect(result.message).toBe(ERRORS.INVALID_CREDENTIALS);
   });
 });
