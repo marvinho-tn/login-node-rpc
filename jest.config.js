8f15eabfb8619967594754bf22a854e667855a1e
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-11-28 18:23:58 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+
 module.exports = {
   testEnvironment: "node",
+  testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    '^.+\\.ts$': 'ts-jest',
   },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
