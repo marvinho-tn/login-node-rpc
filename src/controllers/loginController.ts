703b16015427c4e13b75219d4ebbcf87bf82179f
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-11-28 13:09:55 +0000

    tratamento de exceção no controller de login

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,12 +1,26 @@
 import * as grpc from '@grpc/grpc-js';
 import LoginService from '../services/loginService';
+import { LOGIN_PROCESS_ERROR, LOGIN_SERVICE_ERROR } from '../utils/constants';
 
 export default function login(
   call: grpc.ServerUnaryCall<{ username: string; password: string }, {}>,
   callback: grpc.sendUnaryData<{ message: string; success: boolean }>
 ): void {
   const service = new LoginService();
-  const response = service.login(call.request.username, call.request.password);
 
-  callback(null, { message: response.message, success: response.success });
+  try {
+    const response = service.login(
+      call.request.username,
+      call.request.password
+    );
+
+    callback(null, response);
+  } catch (error) {
+    console.error(LOGIN_SERVICE_ERROR, error);
+
+    callback({
+      code: grpc.status.INTERNAL,
+      message: LOGIN_PROCESS_ERROR,
+    } as grpc.ServiceError, null);
+  }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
 export const PROTO_PATH = './src/protos/login.proto';
 export const LOGIN_SUCCESSFUL_MESSAGE = 'Login successful';
 export const INVALID_CREDENTIALS_MESSAGE = 'Invalid credentials';
+export const LOGIN_SERVICE_ERROR = 'Erro no serviço de login:';
+export const LOGIN_PROCESS_ERROR = 'Erro ao processar o login. Tente novamente mais tarde.';
